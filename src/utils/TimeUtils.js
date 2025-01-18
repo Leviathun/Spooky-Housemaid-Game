f34333608ac4ca871b80abb680f6979e5168258d
Leviathun <153574902+Leviathun@users.noreply.github.com>
2025-01-18 21:07:23 +0700

    7.Create TimeUtila.js

---
+++ src/utils/TimeUtils.js
@@ -0,0 +1,6 @@
+export function formatTime(milliseconds) {
+    const totalSeconds = Math.floor(milliseconds / 1000);
+    const minutes = String(Math.floor(totalSeconds / 60)).padStart(2, '0');
+    const seconds = String(totalSeconds % 60).padStart(2, '0');
+    return `${minutes}:${seconds}`;
+}
